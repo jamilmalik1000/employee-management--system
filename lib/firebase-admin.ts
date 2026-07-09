c22e47c1db7bb53c5e6e32dfc530939380aa1276
Jamil Malik <jamilmalik2500@gmail.com>
2026-07-09 16:56:37 +0500

    Use Firebase Admin environment variables

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,16 +1,35 @@
+// import { cert, getApps, initializeApp } from "firebase-admin/app";
+// import { getAuth } from "firebase-admin/auth";
+// import { getFirestore } from "firebase-admin/firestore";
+// import type { ServiceAccount } from "firebase-admin/app";
+
+// import serviceAccount from "../keys/firebase-admin.json";
+
+// const adminApp =
+//   getApps().length === 0
+//     ? initializeApp({
+//         credential: cert(serviceAccount as ServiceAccount),
+//       })
+//     : getApps()[0];
+
+// export const adminAuth = getAuth(adminApp);
+// export const adminDb = getFirestore(adminApp);
+
+
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import type { ServiceAccount } from "firebase-admin/app";
-
-import serviceAccount from "../keys/firebase-admin.json";
 
 const adminApp =
-  getApps().length === 0
-    ? initializeApp({
-        credential: cert(serviceAccount as ServiceAccount),
-      })
-    : getApps()[0];
+  getApps().length > 0
+    ? getApps()[0]
+    : initializeApp({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        }),
+      });
 
 export const adminAuth = getAuth(adminApp);
 export const adminDb = getFirestore(adminApp);
